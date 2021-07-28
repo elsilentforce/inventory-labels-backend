7a29987d30116baf8bfcf19dd4d4c1c750821472
Patricio Tapia <native.devil@gmail.com>
2021-07-28 18:45:28 -0400

    REFACT: Routes as resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get '/storages', to: 'storages#index'
+      # get '/storages', to: 'storages#index'
+      resources :storages, only: [:index]
     end
   end
 end
