26294d08f576eb79bb0d8492be12913dbe04815b
Patricio Tapia <native.devil@gmail.com>
2021-07-28 18:46:06 -0400

    REFACT: Controller code

--- app/controllers/api/v1/storages_controller.rb
+++ app/controllers/api/v1/storages_controller.rb
@@ -1,20 +1,19 @@
 module Api
   module V1
     class StoragesController < ApplicationController
-    
       def index
         @storages = Storage.all
         render json: @storages
       end
     
-      private 
-        def set_storage
-          @storage = Storage.find(params[:id])
-        end
-    
-        def storage_params
-          params.require(:storage).permit(:name, :friendly_id)
-        end
+      private
+      def set_storage
+        @storage = Storage.find(params[:id])
+      end
+  
+      def storage_params
+        params.require(:storage).permit(:name, :friendly_id)
+      end
     end
   end
 end
