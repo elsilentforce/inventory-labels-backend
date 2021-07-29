b142b2fb723feaa925fdd0dcd8957bc559e8772e
Patricio Tapia <native.devil@gmail.com>
2021-07-29 19:59:44 -0400

    ADD: Scopes and Filterable concern

--- app/models/storage.rb
+++ app/models/storage.rb
@@ -1,3 +1,5 @@
 class Storage < ApplicationRecord
+  include Filterable
   has_many :items, dependent: :destroy
+  scope :by_description, -> (description) { where("description ILIKE '#{ description }%'") }
 end
